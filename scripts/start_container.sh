eacd2fe8226c3d44d1a43412de44b6df1bf61251
Deena77 <114674404+Deena77@users.noreply.github.com>
2024-08-16 17:46:19 +0530

    codedeploy commit-2

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull deena7/simple-python-app
+sudo docker pull deena7/simple-python-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 deena7/simple-python-app
+sudo docker run -d -p 5000:5000 deena7/simple-python-app
